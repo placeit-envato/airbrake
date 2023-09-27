682468070c3aca1354b9b08f98cd55a72a315e24
David Verba <github@davidverba.com>
2023-09-27 17:12:23 -0400

    Resolve issue affecting some local devs

--- lib/airbrake/rails/app.rb
+++ lib/airbrake/rails/app.rb
@@ -45,7 +45,11 @@ module Airbrake
             # Rails 7+ the AST for the route is not kept in memory anymore.
             #
             # See: https://github.com/rails/rails/pull/43006#discussion_r783895766
-            next if route.path.spec.any?(ActionDispatch::Journey::Nodes::Star)
+            #
+            # Placeit is not on Rails 7 and this line in the original gem caused
+            # issues for some devs in local. Reverting this line to its original
+            # form in earlier gem versions.
+            next if route.respond_to?(:glob?) && route.glob?
 
             path =
               if engine == ::Rails.application
